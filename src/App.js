7debed48acc9321cfb683d4ab16da42c808a6016
rajan448 <rajan448@users.noreply.github.com>
2023-08-22 15:58:59 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './style.css';
-import {HelmetProvider, Helmet} from 'react-helmet-async';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 
 export default function App() {
   return (
     <div>
       <Helmet prioritizeSeoTags>
-        <meta property="og:type" content="website">
-        <meta property="og:title" content="Airtel Black">
-        <meta property="og:description" content="Airtel Black's description of meta tag test website">
-        <meta property="og:image" content="https://assets.airtel.in/static-assets/safo/dth/images/jpg/img_stb_2.jpg">
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Airtel Black" />
+        <meta
+          property="og:description"
+          content="Airtel Black's description of meta tag test website"
+        />
+        <meta
+          property="og:image"
+          content="https://assets.airtel.in/static-assets/safo/dth/images/jpg/img_stb_2.jpg"
+        />
       </Helmet>
+
+      <h1>Meta async </h1>
     </div>
   );
 }
